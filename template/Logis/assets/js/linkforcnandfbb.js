f71fe9eb0c476cf938144009af4c5c654eeada20
DivyaMaryFrancis <98161832+DivyaMaryFrancis@users.noreply.github.com>
2024-05-02 13:44:29 +0530

    created scripy file for link to feedback and ccall now

---
+++ template/Logis/assets/js/linkforcnandfbb.js
@@ -0,0 +1,20 @@
+
+function callNowClicked(button) {
+  var name = button.parentElement.querySelector('.name-stretched-link').textContent;
+  // Add your JavaScript code for the "Call Now" button functionality here
+  // You can use the name parameter
+  window.location.href = 'profile.html';
+  // Then, redirect to the profile.html page or perform other actions
+}
+
+function feedbackClicked(button) {
+  var name = button.parentElement.querySelector('.name-stretched-link').textContent;
+  // Add your JavaScript code for the "Feedback" button functionality here
+  // You can use the name parameter
+  window.location.href = 'feedback.html';
+  // Then, redirect to the feedback.html page or perform other actions
+}
+
+
+ 
+  
